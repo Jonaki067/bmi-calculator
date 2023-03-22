e1a76e957e9df96e27479a0f9c39463feb38e760
JONAKI <nusratjonaki123@gmail.com>
2023-03-23 00:02:38 +0600

    One java class is created

---
+++ app/src/main/java/com/github/characterdog/bmicalculator/java1.java
@@ -0,0 +1,26 @@
+package com.github.characterdog.bmicalculator;
+
+public class java1 {
+
+    String s1,s2;
+    int a;
+
+    //Constructor1
+    public java1() {
+
+    }
+
+    //Constructor2
+    public java1(int a) {
+        this.a = a;
+    }
+
+    //Getter & Setter
+    public String getS1() {
+        return s1;
+    }
+
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+}
