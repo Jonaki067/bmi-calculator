19ef34ad9213a77f3328dac5648840a7f91515f1
JONAKI <nusratjonaki123@gmail.com>
2023-03-23 00:36:34 +0600

    Java2 class is created in new branch

---
+++ app/src/main/java/com/github/characterdog/bmicalculator/Java2.java
@@ -0,0 +1,13 @@
+package com.github.characterdog.bmicalculator;
+
+public class Java2 {
+    float b;
+
+    public float getB() {
+        return b;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+}
